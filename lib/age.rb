7e26068313840c8cb98e9e05bdd53f5e6a44e024
sanemat <o.gata.ken@gmail.com>
2013-05-31 14:53:00 +0900

    Use leep year for date compare

--- lib/age.rb
+++ lib/age.rb
@@ -9,7 +9,18 @@ class Age
     @end = options[:end] || DateTime.now.offset(0)
   end
 
+  def years
+    leep_year = 2004
+    end_with_leep_year = DateTime.new(leep_year, @end.month, @end.day, @end.hour, @end.min, @end.sec)
+    birth_with_leep_year = DateTime.new(leep_year, @birth.month, @birth.day, @birth.hour, @birth.min, @birth.sec)
+    if end_with_leep_year < birth_with_leep_year
+      @end.year - @birth.year - 1
+    else
+      @end.year - @birth.year
+    end
+  end
+
   def to_i
-    30
+    years
   end
 end
