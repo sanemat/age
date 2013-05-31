49b629f7a80431bf2d6197dee1943c60098cdfd4
sanemat <o.gata.ken@gmail.com>
2013-05-31 12:45:18 +0900

    Use DateTime in internal

--- lib/age.rb
+++ lib/age.rb
@@ -1,7 +1,12 @@
 require 'age/version'
+require 'date'
 
 class Age
+  attr_accessor :birth, :end
+
   def initialize(options)
+    @birth = options[:birth] || DateTime.now.offset(0)
+    @end = options[:end] || DateTime.now.offset(0)
   end
 
   def to_i
--- test/test_age.rb
+++ test/test_age.rb
@@ -1,10 +1,13 @@
 #coding: utf-8
 require 'minitest/autorun'
-require 'date'
 require 'age'
 
 class TestAge < MiniTest::Unit::TestCase
   def test_sanemat
     assert_equal Age.new(birth: DateTime.new(1982, 7, 16), end: DateTime.new(2013, 5, 24)).to_i, 30
+  end
+
+  def test_ruby
+    assert_equal Age.new(birth: DateTime.new(1993, 2, 24), end: DateTime.new(2013, 5, 31)).to_i, 20
   end
 end
