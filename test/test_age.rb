f7414f9e0a18c56ebab24f1315582a6f6c6431cd
sanemat <o.gata.ken@gmail.com>
2013-06-01 22:03:08 +0900

    Use variable

--- test/test_age.rb
+++ test/test_age.rb
@@ -4,16 +4,22 @@ require 'age'
 require 'coveralls'
 Coveralls.wear!
 
-class TestAge < MiniTest::Test
+class TestEndWithDateTime < MiniTest::Test
+  def setup
+    @sanemat = DateTime.new(1982, 7, 16)
+    @ruby = DateTime.new(1993, 2, 24)
+    @today = DateTime.new(2013, 5, 31)
+  end
+
   def test_sanemat
-    assert_equal Age.new(birth: DateTime.new(1982, 7, 16), end: DateTime.new(2013, 5, 24)).to_i, 30
+    assert_equal Age.new(birth: @sanemat, end: @today).to_i, 30
   end
 
   def test_ruby
-    assert_equal Age.new(birth: DateTime.new(1993, 2, 24), end: DateTime.new(2013, 5, 31)).to_i, 20
+    assert_equal Age.new(birth: @ruby, end: @today).to_i, 20
   end
 
   def test_inspect
-    assert_equal Age.new(birth: DateTime.new(1993, 2, 24), end: DateTime.new(2013, 5, 31)).inspect, '20'
+    assert_equal Age.new(birth: @ruby, end: @today).inspect, '20'
   end
 end
